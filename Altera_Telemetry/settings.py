8f94bb5ee77081357479eb7bb98ff0e5515253ec
sushant-maurya <sushantmaurya52.com>
2025-09-23 10:15:40 +0530

    Set up the app and urls for overall page

--- Altera_Telemetry/settings.py
+++ Altera_Telemetry/settings.py
@@ -35,6 +35,9 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
+    'project',
+    'subscribe',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -58,7 +61,9 @@ ROOT_URLCONF = 'Altera_Telemetry.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
